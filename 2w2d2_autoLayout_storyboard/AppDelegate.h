b8a055321ded5c0e6489d19d4c5cd6a779b017ff
hkimca <harrison.kim.ca@gmail.com>
2017-07-03 23:59:32 -0400

    Initial Commit

---
+++ 2w2d2_autoLayout_storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2w2d2_autoLayout_storyboard
+//
+//  Created by Seantastic31 on 03/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2w2d2_autoLayout_storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2w2d2_autoLayout_storyboard
+//
+//  Created by Seantastic31 on 03/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
